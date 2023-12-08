c90a06fd189e006efca25d05de156b03471ed15c
OttoDyva <cph-ot@cphbusiness.dk>
2023-12-08 11:50:22 +0100

    bille

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,105 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-            System.out.println("Hej farmand!");
-        }
+        System.out.println("Min baby bar' smil, vi' i Milano\n" +
+                "Hun kalder mig for: \"Papi El Capitano\"\n" +
+                "Min brormand, bar' spis\n" +
+                "For ingen skal mangl' noget\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Se mig varm i Ibiza\n" +
+                "Se mig varm i Ibiza, ah-ah\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Se mig varm i Ibiza\n" +
+                "Se mig varm i Ibiza, ah-ah\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Velkommen til bandoen\n" +
+                "Velkommen til bandoen\n" +
+                "Hun kald' mig Papi El Benny Jammo\n" +
+                "Hun si'r jeg' hendes mi amor\n" +
+                "Så jeg si'r til hende:\n" +
+                "\"Te amo\"\n" +
+                "Hun vil aldrig la' mig gå (Aldrig)\n" +
+                "Så jeg ta'r hendе til Milano\n" +
+                "Hun si'r til mig\n" +
+                "Jeg' hendes mi amor\n" +
+                "Så jеg si'r til hende:\n" +
+                "\"Te amo\"\n" +
+                "Min baby bar' smil, vi' i Milano\n" +
+                "Hun kalder mig for: \"Papi El Capitano\"\n" +
+                "Min brormand, bar' spis\n" +
+                "For ingen skal mangl' noget\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Se mig varm i Ibiza\n" +
+                "Se mig varm i Ibiza, ah-ah\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Se mig varm i Ibiza\n" +
+                "Se mig varm i Ibiza, ah-ah\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Jeg' helt blæst\n" +
+                "La' vær' at prøv' og komm' og ryk' i mig, ih-ih\n" +
+                "Det' ikk' Tony Montana\n" +
+                "Men the world is mine\n" +
+                "Og min Colombiana, hun er på 2C-B, eh-eh\n" +
+                "Så hun arbejder på mig\n" +
+                "Som var hun Super Saiyajin\n" +
+                "Gi' mig - gi' mig en giz ogs'\n" +
+                "Ligeglad hvilken farve\n" +
+                "Bar' der' dizz nok, åh-åh\n" +
+                "Tro - tro mig, der' skeez nok\n" +
+                "Efterhånden blevet normalt at de kigger, åh-åh\n" +
+                "Kold - kold, som freezer\n" +
+                "Ikk' noget passport eller Visa, ah-ah\n" +
+                "Ah-ah\n" +
+                "(Chuba, chuba)\n" +
+                "Min baby bar' smil, vi' i Milano\n" +
+                "Hun kalder mig for: \"Papi El Capitano\"\n" +
+                "Min brormand, bar' spis\n" +
+                "For ingen skal mangl' noget\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Se mig varm i Ibiza\n" +
+                "Se mig varm i Ibiza, ah-ah\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Se mig varm i Ibiza\n" +
+                "Se mig varm i Ibiza, ah-ah\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Så' vi ude på tur igen (Yeah)\n" +
+                "Man henter flousene ind (Yeah)\n" +
+                "Gang, ligesom hooligan (Yeah)\n" +
+                "Lav peng' og så forsvind (Shh)\n" +
+                "De ka' ikk' kom' tæt på\n" +
+                "Hvem ka' nå os? Ikk' nogen\n" +
+                "Fashion dresscode\n" +
+                "Hun danser for peso (Wåh-ah)\n" +
+                "Prøver bare stabl' noget (Stabl', stabl', stabl')\n" +
+                "Vi' foran, de' bagud\n" +
+                "Ta'r hende til Milano\n" +
+                "Diamanter ligner' der' vand på, åh-åh-åh (Splash, splash)\n" +
+                "Prøver bare stabl' noget (Ah)\n" +
+                "Vi' foran, de' bagud (Bagud)\n" +
+                "Ta'r hende til Milano (Ah-ah)\n" +
+                "Si'r: \"Velkommen til bando\", åh-åh-åh\n" +
+                "Min baby bar' smil, vi' i Milano\n" +
+                "Hun kalder mig for: \"Papi El Capitano\"\n" +
+                "Min brormand, bar' spis\n" +
+                "For ingen skal mangl' noget\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Se mig varm i Ibiza\n" +
+                "Se mig varm i Ibiza, ah-ah\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n" +
+                "Se mig varm i Ibiza\n" +
+                "Se mig varm i Ibiza, ah-ah\n" +
+                "Vi stabler bar' skeez\n" +
+                "Velkommen til bandoen\n");
     }
 }
