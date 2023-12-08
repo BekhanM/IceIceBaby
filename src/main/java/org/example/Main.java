8b3950399726aac306465b43d939163bc2e92113
RyanZachariasen <143599102+RyanZachariasen@users.noreply.github.com>
2023-12-08 11:49:42 +0100

    hello

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.printf(" welcome!");
 
-        for (int i = 1; i <= 1000000000; i++) {
-
-
-            System.out.println("eat ass die fast");
-        }
     }
 }
